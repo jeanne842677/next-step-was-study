0ebf0bfd6b88d5fa548853b94595fd04fce7ef96
jiyeong-dt <imjee02@gmail.com>
2024-01-07 22:19:20 +0900

    feat: 프로젝트 필요한 파일 setting

---
+++ src/main/java/webserver/WebApplicationServer.java
@@ -0,0 +1,16 @@
+package webserver;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebApplicationServer {
+    private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String args[]) throws Exception {
+
+    }
+}

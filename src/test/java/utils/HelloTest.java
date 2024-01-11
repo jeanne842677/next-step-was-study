acf858f66a4defd3e391dc5bdb42ca66ff8fbad6
jeanne8426 <jeanne842677@gmail.com>
2024-01-11 21:19:43 +0900

    feat: 헬로 월드 찍어보기

---
+++ src/test/java/utils/HelloTest.java
@@ -0,0 +1,12 @@
+package utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+    @DisplayName("헬로월드 테스트")
+    @Test
+    void helloworldTest() {
+        System.out.println("hello world");
+    }
+}
